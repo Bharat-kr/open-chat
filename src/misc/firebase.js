48c7d334424328e32f72cb9ed890c974389b4a4d
Bharat Kumar <kumarbharat661@gmail.com>
2021-09-02 10:54:11 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBCzPyaKnWAI652UKNTt29mwTTqbCWQzVw',
+  authDomain: 'open-chat-fe011.firebaseapp.com',
+  projectId: 'open-chat-fe011',
+  storageBucket: 'open-chat-fe011.appspot.com',
+  messagingSenderId: '322661198746',
+  appId: '1:322661198746:web:61c98b63be5988534a6823',
+};
+
+const app = firebase.initializeApp(config);
