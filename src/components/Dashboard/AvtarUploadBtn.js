00ef3d65557354b0c3319d10bb6b1f7f7946ff7a
Bharat Kumar <kumarbharat661@gmail.com>
2021-09-07 21:27:37 +0530

    uploaded user avatar

--- src/components/Dashboard/AvtarUploadBtn.js
+++ src/components/Dashboard/AvtarUploadBtn.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Alert, Button, Modal } from 'rsuite';
 import AvatarEditor from 'react-avatar-editor';
 import { useModalState } from '../../misc/custom-hooks';
+import { database, storage } from '../../misc/firebase';
+import { useProfile } from '../../context/profile.context';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
 
 const isValidFile = file => acceptedFileTypes.includes(file.type);
 
+const getBlob = canvas => {
+    return new Promise((resolve, reject) => {
+        canvas.toBlob(blob => {
+            if (blob) {
+                resolve(blob);
+            } else {
+                reject(new Error('File Process Error'));
+            }
+        });
+    });
+};
+
 const AvtarUploadBtn = () => {
     const { isOpen, open, close } = useModalState();
+    const { profile } = useProfile();
     const [image, setImg] = useState(null);
+    const [isLoading , setIsLoading] = useState(false);
+    const avatarEditorRef = useRef();
 
     const onFileInputChange = ev => {
         const currFiles = ev.target.files;
@@ -23,6 +40,35 @@ const AvtarUploadBtn = () => {
             } else {
                 Alert.warning(`Wrong file type ${file.type}`, 4000);
             }
+        }
+    };
+
+    const onUploadClick = async () => {
+        const canvas = avatarEditorRef.current.getImageScaledToCanvas();
+        setIsLoading(true);
+        try {
+            const blob = await getBlob(canvas);
+
+            const avatarFileRef = storage
+                .ref(`/profile/${profile.uid}`)
+                .child('avatar');
+
+            const uploadAvatarResult = await avatarFileRef.put(blob, {
+                cacheControl: `public, max-age=${3600 * 24 * 3}`,
+            });
+
+            const downloadUrl = uploadAvatarResult.ref.getDownloadURL();
+
+            const userAvatarRef = database
+                .ref(`/profiles/${profile.uid}`)
+                .child('avatar');
+
+            await userAvatarRef.set(downloadUrl);
+            setIsLoading(false);
+            Alert.info('Avatar has been uploaded', 4000);
+        } catch (err) {
+            setIsLoading(false);
+            Alert.error(err.message, 4000);
         }
     };
 
@@ -49,19 +95,21 @@ const AvtarUploadBtn = () => {
                 </Modal.Header>
                 <Modal.Body>
                     <div className="d-flex justify-content-center align-items-center h-100">
-                    {image && (
-                        <AvatarEditor
-                            image={image}
-                            width={200}
-                            height={200}
-                            border={10}
-                            borderRadius={100}
-                            rotate={0}
-                        />
-                    )}</div>
+                        {image && (
+                            <AvatarEditor
+                                ref={avatarEditorRef}
+                                image={image}
+                                width={200}
+                                height={200}
+                                border={10}
+                                borderRadius={100}
+                                rotate={0}
+                            />
+                        )}
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button block appearance="ghost">
+                    <Button block appearance="ghost" onClick={onUploadClick} disabled={isLoading}>
                         Upload new Avatar
                     </Button>
                 </Modal.Footer>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,16 +1,18 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import 'firebase/compat/database'
+import 'firebase/compat/database';
+import 'firebase/compat/storage';
 
 const config = {
-  apiKey: 'AIzaSyBCzPyaKnWAI652UKNTt29mwTTqbCWQzVw',
-  authDomain: 'open-chat-fe011.firebaseapp.com',
-  projectId: 'open-chat-fe011',
-  storageBucket: 'open-chat-fe011.appspot.com',
-  messagingSenderId: '322661198746',
-  appId: '1:322661198746:web:61c98b63be5988534a6823',
+    apiKey: 'AIzaSyBCzPyaKnWAI652UKNTt29mwTTqbCWQzVw',
+    authDomain: 'open-chat-fe011.firebaseapp.com',
+    projectId: 'open-chat-fe011',
+    storageBucket: 'open-chat-fe011.appspot.com',
+    messagingSenderId: '322661198746',
+    appId: '1:322661198746:web:61c98b63be5988534a6823',
 };
 
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
