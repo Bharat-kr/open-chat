3453db60a9586c85f241d34ba27395e85dbc9d73
Bharat Kumar <kumarbharat661@gmail.com>
2021-09-12 22:32:24 +0530

    Added edit room drawer

--- src/components/EditableInput.js
+++ src/components/EditableInput.js
@@ -7,6 +7,7 @@ const EditableInput = ({
     label = null,
     placeholder = 'write your value',
     emptyMsg = 'Input is empty',
+    wrapperClassName="",
     ...inputProps
 }) => {
     const [input, setInput] = useState(initialValue);
@@ -36,7 +37,7 @@ const EditableInput = ({
     };
 
     return (
-        <div>
+        <div className={wrapperClassName}>
             {label}
             <InputGroup>
                 <Input
