29c6bdf1a88e828178bd199533c775d89bc0fee4
Bharat Kumar <kumarbharat661@gmail.com>
2021-09-14 12:28:59 +0530

    grouped messages by date

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
 import { auth, database, storage } from '../../../misc/firebase';
-import { transformToArrWithId } from '../../../misc/helpers';
+import { groupBy, transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
 const Messages = () => {
@@ -113,19 +113,39 @@ const Messages = () => {
         [chatId, messages]
     );
 
+    const renderMessages = () => {
+        const groups = groupBy(messages, item =>
+            new Date(item.createdAt).toDateString()
+        );
+
+        let items = [];
+
+        Object.keys(groups).forEach(date => {
+            items.push(
+                <li key={date} className="text-center mb-1 padded">
+                    {date}
+                </li>
+            );
+
+            const msgs = groups[date].map(msg => (
+                <MessageItem
+                    key={msg.id}
+                    message={msg}
+                    handleAdmin={handleAdmin}
+                    handleLike={handleLike}
+                    handleDelete={handleDelete}
+                />
+            ));
+
+            items.push(...msgs);
+        });
+        return items;
+    };
+
     return (
         <ul className="msg-list custom-scroll">
             {isChatEmpty && <li>No messages yet</li>}
-            {canShowMessages &&
-                messages.map(msg => (
-                    <MessageItem
-                        key={msg.id}
-                        message={msg}
-                        handleAdmin={handleAdmin}
-                        handleLike={handleLike}
-                        handleDelete={handleDelete}
-                    />
-                ))}
+            {canShowMessages && renderMessages()}
         </ul>
     );
 };
